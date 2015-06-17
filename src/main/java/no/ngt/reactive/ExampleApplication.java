451170500f9ea59565a65e0dd346f01662f7e67a
Eirik Tenold <eirik@relativt.net>
2015-06-17 17:41:52 +0200

    Initial setup

---
+++ src/main/java/no/ngt/reactive/ExampleApplication.java
@@ -0,0 +1,11 @@
+package no.ngt.reactive;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Environment;
+
+public class ExampleApplication extends Application<ExampleConfig> {
+	@Override
+	public void run(ExampleConfig configuration, Environment environment) throws Exception {
+
+	}
+}
---
+++ src/main/java/no/ngt/reactive/ExampleConfig.java
@@ -0,0 +1,6 @@
+package no.ngt.reactive;
+
+import io.dropwizard.Configuration;
+
+public class ExampleConfig extends Configuration {
+}
